ce7bf7ffcf3a62b3289a6f1b3aff8a23f0dad055
Luigi Kapaj <Luigi.Kapaj@NYTimes.com>
2015-04-01 12:11:51 -0400

    keep statements prepared

--- dbhammer.go
+++ dbhammer.go
@@ -25,12 +25,20 @@ export MYSQL_TEST_PROT=tcp
 export MYSQL_TEST_ADDR=localhost
 export MYSQL_TEST_CONCURRENT=1
 export MYSQL_TEST_PORT=3306
+
+intentionally mimicks github.com/go-sql-driver/mysql
 */
 
 var (
-	// NumTries int = 100
-	TraceLog *log.Logger
-	// Tags     []string = []string{"tag #1", "tag #2", "tag #3"}
+	TraceLog       *log.Logger
+	CreateTag      *sql.Stmt
+	CreatePeople   *sql.Stmt
+	InsertTag      *sql.Stmt
+	SelectTagError *sql.Stmt
+	InsertPeople   *sql.Stmt
+	SelectPeople   *sql.Stmt
+	SelectInSelect *sql.Stmt
+	SelectCount    *sql.Stmt
 )
 
 func InsertRow(stmt *sql.Stmt, someTag string, wg *sync.WaitGroup) {
@@ -44,6 +52,71 @@ func InsertRow(stmt *sql.Stmt, someTag string, wg *sync.WaitGroup) {
 		TraceLog.Println(err.Error())
 	}
 	wg.Done()
+}
+
+func PrepareAll(db *sql.DB) {
+	var err error
+	CreateTag, err = db.Prepare("CREATE TABLE tagging (tag VARCHAR(50) PRIMARY KEY);")
+	if err != nil {
+		TraceLog.Fatal(err.Error())
+	}
+	CreatePeople, err = db.Prepare("CREATE TABLE people (name VARCHAR(50) PRIMARY KEY, tag VARCHAR(50) NOT NULL, FOREIGN KEY (tag) REFERENCES tagging(tag));")
+	if err != nil {
+		TraceLog.Fatal(err.Error())
+	}
+	InsertTag, err = db.Prepare("INSERT INTO tagging (tag) VALUES (?);")
+	if err != nil {
+		TraceLog.Fatal(err.Error())
+	}
+	SelectTagError, err = db.Prepare("SELECT tag FROM tagging WHERE tag = ?;")
+	if err != nil {
+		TraceLog.Fatal(err.Error())
+	}
+	InsertPeople, err = db.Prepare("INSERT INTO people (name, tag) VALUES (?,?);")
+	if err != nil {
+		TraceLog.Fatal(err.Error())
+	}
+	SelectPeople, err = db.Prepare("SELECT name, tag FROM people WHERE tag = ? LIMIT ?;")
+	if err != nil {
+		TraceLog.Fatal(err.Error())
+	}
+	SelectInSelect, err = db.Prepare("SELECT name, tag FROM people WHERE tag IN (SELECT tag FROM tagging) LIMIT 0,?;")
+	if err != nil {
+		TraceLog.Fatal(err.Error())
+	}
+	SelectCount, err = db.Prepare("SELECT count(*) FROM people;")
+	if err != nil {
+		TraceLog.Fatal(err.Error())
+	}
+
+}
+
+func CloseAll(db *sql.DB) {
+	var err error
+	if err = CreateTag.Close(); err != nil {
+		TraceLog.Fatal(err.Error())
+	}
+	if err = CreatePeople.Close(); err != nil {
+		TraceLog.Fatal(err.Error())
+	}
+	if err = InsertTag.Close(); err != nil {
+		TraceLog.Fatal(err.Error())
+	}
+	if err = SelectTagError.Close(); err != nil {
+		TraceLog.Fatal(err.Error())
+	}
+	if err = InsertPeople.Close(); err != nil {
+		TraceLog.Fatal(err.Error())
+	}
+	if err = SelectPeople.Close(); err != nil {
+		TraceLog.Fatal(err.Error())
+	}
+	if err = SelectInSelect.Close(); err != nil {
+		TraceLog.Fatal(err.Error())
+	}
+	if err = SelectCount.Close(); err != nil {
+		TraceLog.Fatal(err.Error())
+	}
 }
 
 func main() {
@@ -94,6 +167,7 @@ func main() {
 	if err != nil {
 		TraceLog.Fatal(err.Error())
 	}
+	PrepareAll(db)
 
 	TraceLog.Println("Initializing tables")
 	_, err = db.Exec("DROP TABLE IF EXISTS people;")
@@ -105,55 +179,30 @@ func main() {
 		TraceLog.Fatal(err.Error())
 	}
 
-	stmt, err := db.Prepare("CREATE TABLE tagging (tag VARCHAR(50) PRIMARY KEY);")
+	_, err = CreateTag.Exec()
 	if err != nil {
-		TraceLog.Fatal(err.Error())
-	}
-	_, err = stmt.Exec()
-	if err != nil {
-		TraceLog.Fatal(err.Error())
-	}
-	if err = stmt.Close(); err != nil {
 		TraceLog.Fatal(err.Error())
 	}
 
-	stmt, err = db.Prepare("CREATE TABLE people (name VARCHAR(50) PRIMARY KEY, tag VARCHAR(50) NOT NULL, FOREIGN KEY (tag) REFERENCES tagging(tag));")
+	_, err = CreatePeople.Exec()
 	if err != nil {
-		TraceLog.Fatal(err.Error())
-	}
-	_, err = stmt.Exec()
-	if err != nil {
-		TraceLog.Fatal(err.Error())
-	}
-	if err = stmt.Close(); err != nil {
 		TraceLog.Fatal(err.Error())
 	}
 
 	TraceLog.Println("Populating tags")
-	stmt, err = db.Prepare("INSERT INTO tagging (tag) VALUES (?);")
-	if err != nil {
-		TraceLog.Fatal(err.Error())
-	}
 	for i := 0; i < len(Tags); i++ {
 		someTag := Tags[i]
 		TraceLog.Printf("Inserting tag: \"%s\"\n", someTag)
-		_, err = stmt.Exec(someTag)
+		_, err = InsertTag.Exec(someTag)
 		if err != nil {
 			TraceLog.Println(err.Error())
 		}
 	}
-	if err = stmt.Close(); err != nil {
-		TraceLog.Fatal(err.Error())
-	}
 
 	if *forceSqlError {
 		TraceLog.Println("Force Query() error")
-		stmt, err = db.Prepare("SELECT tag FROM tagging WHERE tag = ?;")
-		if err != nil {
-			TraceLog.Fatal(err.Error())
-		}
 
-		rows, err := stmt.Query(Tags[0], "error param")
+		rows, err := SelectTagError.Query(Tags[0], "error param")
 		if err != nil {
 			TraceLog.Println(err.Error())
 		} else {
@@ -172,17 +221,8 @@ func main() {
 			}
 		}
 
-		if err = stmt.Close(); err != nil {
-			TraceLog.Fatal(err.Error())
-		}
-
 		TraceLog.Println("Force Scan() error")
-		stmt, err = db.Prepare("SELECT tag FROM tagging WHERE tag = ?;")
-		if err != nil {
-			TraceLog.Fatal(err.Error())
-		}
-
-		rows, err = stmt.Query(Tags[0])
+		rows, err = SelectTagError.Query(Tags[0])
 		if err != nil {
 			TraceLog.Println(err.Error())
 		}
@@ -200,35 +240,21 @@ func main() {
 			TraceLog.Fatal(err.Error())
 		}
 
-		if err = stmt.Close(); err != nil {
-			TraceLog.Fatal(err.Error())
-		}
 	}
 
 	TraceLog.Println("Populating table")
 	var wg sync.WaitGroup
-	stmt, err = db.Prepare("INSERT INTO people (name, tag) VALUES (?,?);")
-	if err != nil {
-		TraceLog.Fatal(err.Error())
-	}
 	for i := 0; i < *tries; i++ {
 		wg.Add(1)
-		go InsertRow(stmt, Tags[i%len(Tags)], &wg)
+		go InsertRow(InsertPeople, Tags[i%len(Tags)], &wg)
 	}
 	wg.Wait()
-	if err = stmt.Close(); err != nil {
-		TraceLog.Fatal(err.Error())
-	}
 
 	TraceLog.Println("Reading table by tag")
-	stmt, err = db.Prepare("SELECT name, tag FROM people WHERE tag = ? LIMIT ?;")
-	if err != nil {
-		TraceLog.Fatal(err.Error())
-	}
 	for i := 0; i < len(Tags); i++ {
 		someTag := Tags[i]
 		TraceLog.Printf("Reading %s...\n", Tags[i])
-		rows, err := stmt.Query(someTag, *tries)
+		rows, err := SelectPeople.Query(someTag, *tries)
 		if err != nil {
 			TraceLog.Println(err.Error())
 		}
@@ -252,16 +278,9 @@ func main() {
 			TraceLog.Fatal(err.Error())
 		}
 	}
-	if err = stmt.Close(); err != nil {
-		TraceLog.Fatal(err.Error())
-	}
 
 	TraceLog.Println("Inefficient query all")
-	stmt, err = db.Prepare("SELECT name, tag FROM people WHERE tag IN (SELECT tag FROM tagging) LIMIT 0,?;")
-	if err != nil {
-		TraceLog.Fatal(err.Error())
-	}
-	rows, err := stmt.Query(*tries)
+	rows, err := SelectInSelect.Query(*tries)
 	if err != nil {
 		TraceLog.Println(err.Error())
 	}
@@ -278,27 +297,18 @@ func main() {
 	if err = rows.Close(); err != nil {
 		TraceLog.Fatal(err.Error())
 	}
-	if err = stmt.Close(); err != nil {
-		TraceLog.Fatal(err.Error())
-	}
 
 	TraceLog.Println("Reading table count")
-	stmt, err = db.Prepare("SELECT count(*) FROM people;")
-	if err != nil {
-		TraceLog.Fatal(err.Error())
-	}
 	var query_count int64
-	err = stmt.QueryRow().Scan(&query_count)
+	err = SelectCount.QueryRow().Scan(&query_count)
 	if err != nil {
 		TraceLog.Println(err.Error())
 	}
 	TraceLog.Printf("Total row count: %d\n", query_count)
-	if err = stmt.Close(); err != nil {
-		TraceLog.Fatal(err.Error())
-	}
 
+	// do later prepares her on out
 	TraceLog.Println("Dropping tables")
-	stmt, err = db.Prepare("DROP TABLE people;")
+	stmt, err := db.Prepare("DROP TABLE people;")
 	if err != nil {
 		TraceLog.Fatal(err.Error())
 	}
@@ -326,5 +336,6 @@ func main() {
 		TraceLog.Fatal(err.Error())
 	}
 
+	CloseAll(db)
 	TraceLog.Println("Test done")
 }
